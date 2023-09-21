b19e82d128363e6ad84f9c8ef442ecd86749804a
Faisal Khan <faisaldev404@gmail.com>
2023-09-21 17:31:50 +0500

    style: single product page styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./components/Home/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 import ProductsPage from "./components/Products/ProductsPage";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Navbar />
       <main>
         {/* <HomePage /> */}
-        <ProductsPage />
+        {/* <ProductsPage /> */}
+        <SingleProduct />
       </main>
     </div>
   );
--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import "./SingleProduct.css";
+
+const product = {
+  id: 1,
+  title: "Product Title",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
+  price: 9.99,
+  images: [
+    "https://via.placeholder.com/500x500?text=Product+Image+1",
+    "https://via.placeholder.com/500x500?text=Product+Image+2",
+    "https://via.placeholder.com/500x500?text=Product+Image+3",
+    "https://via.placeholder.com/500x500?text=Product+Image+4",
+  ],
+  stock: 10,
+};
+
 const SingleProduct = () => {
-  return <section></section>;
+  const [selectedImage, setSelectedImage] = useState(0);
+  return (
+    <section className="align_center single_product">
+      <div className="align_center">
+        <div className="single_product_thumbnails">
+          {product.images.map((image, index) => (
+            <img
+              src={image}
+              alt={product.title}
+              key={index}
+              className={selectedImage === index ? "selected_image" : ""}
+              onClick={() => setSelectedImage(index)}
+            />
+          ))}
+        </div>
+        <img
+          src={product.images[selectedImage]}
+          alt={product.title}
+          className="single_product_display"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SingleProduct;
