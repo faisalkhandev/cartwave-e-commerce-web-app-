ab3b093b09ea5317560c65936bd1d5bc2367e061
Faisal Khan <faisaldev404@gmail.com>
2023-09-25 12:02:32 +0500

    feat: login form validation

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,43 +1,45 @@
-import { useRef } from "react";
+import { useForm } from "react-hook-form";
+
 import "./Login.css";
 
 const Login = () => {
-  const passwordRef = useRef(null);
+  const { register, handleSubmit, formState } = useForm();
+  console.log(formState.errors);
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form className="authentication_form" onSubmit={handleSubmit()}>
         <h2>Login Form </h2>
         <div className="form_inputs">
           <div>
-            <label htmlFor="name">Name</label>
+            <label htmlFor="email">Email</label>
             <input
               type="text"
-              id="name"
+              id="email"
               className="form_text_input"
-              placeholder="Enter your name"
+              placeholder="Enter your email"
+              {...register("email", {
+                required: true,
+              })}
             />
+            {formState.errors.email?.type === "required" && (
+              <emp className="form_error">Please Enter your Email</emp>
+            )}
           </div>
           <div>
-            <label htmlFor="phone">Phone</label>
+            <label htmlFor="password">Password</label>
             <input
               type="password"
-              ref={passwordRef}
-              id="phone"
+              id="password"
               className="form_text_input"
-              placeholder="Enter your phone number"
+              placeholder="Enter your password"
+              {...register("password", {
+                required: true,
+                minLength: 3,
+              })}
             />
-            <button
-              type="button"
-              onClick={() => (passwordRef.current.type = "password")}
-            >
-              Hide Password
-            </button>
-            <button
-              type="button"
-              onClick={() => (passwordRef.current.type = "text")}
-            >
-              Show Password
-            </button>
+            {formState.errors.password?.type === "minLength" && (
+              <emp className="form_error">Password should be 8 characters</emp>
+            )}
           </div>
           <button type="submit" className="search_button submit_form">
             Submit
