9a6905e381f00023011108b6802b87965746dc83
Faisal Khan <faisaldev404@gmail.com>
2023-10-05 11:54:39 +0500

    bug: footer fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,29 +4,15 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="row primary">
-          <div className="column about">
-            <h3>Connect</h3>
-            <p>
-              CartWave: Your ultimate E-Commerce solution with React.js and
-              Node.js, offering seamless shopping, authentication, and more.
-              🚀🛒
-            </p>
-          </div>
-
-          <div className="column subscribe">
-            <h3>Newsletter</h3>
-            <div>
-              <input type="email" placeholder="Enter your E-mail" />
-              <button className="subscribe_btn">Subscribe</button>
-            </div>
-          </div>
-        </div>
         <div className="row copyright">
           <div className="footer-menu">
             <h4 style={{ color: "white" }}>
               Developed with ♥ by{" "}
-              <a href="https://www.linkedin.com/in/faisalkhandev/">
+              <a
+                href="https://www.linkedin.com/in/faisalkhandev/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Faisal Khan 😊
               </a>
             </h4>
